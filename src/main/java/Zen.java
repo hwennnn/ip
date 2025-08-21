e569deb9192a2ce187cb5a87aae2869dbc9d614a
Houman <hwendev@gmail.com>
2025-08-21 11:19:52 +0800

    feat: complete level 1

--- src/main/java/Zen.java
+++ src/main/java/Zen.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Zen {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Zen");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        
+        while (true) {
+            String input = scanner.nextLine().trim();
+            
+            System.out.println("____________________________________________________________");
+            
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        
+        scanner.close();
     }
 }
