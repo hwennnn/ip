d1d75a50d43b38922e5711a5d734bbde4e2307f7
Houman <hwendev@gmail.com>
2025-08-30 02:06:15 +0800

    Add j-unit tests for parser and task list

---
+++ src/test/java/zen/command/ParserTest.java
@@ -0,0 +1,34 @@
+package zen.command;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import zen.command.Parser;
+
+/**
+ * JUnit tests for the Parser class
+ */
+public class ParserTest {
+
+    @Test
+    public void parseCommand_basicCommands_returnsCorrectCommandType() {
+        // Test basic commands
+        assertEquals(Parser.CommandType.BYE, Parser.parseCommand("bye"));
+        assertEquals(Parser.CommandType.LIST, Parser.parseCommand("list"));
+        assertEquals(Parser.CommandType.TODO, Parser.parseCommand("todo read book"));
+        assertEquals(Parser.CommandType.MARK, Parser.parseCommand("mark 1"));
+        assertEquals(Parser.CommandType.UNKNOWN, Parser.parseCommand("invalid"));
+        assertEquals(Parser.CommandType.EMPTY, Parser.parseCommand(null));
+    }
+
+    @Test
+    public void parseTaskIndex_validAndInvalidInputs_returnsCorrectResult() {
+        // Test valid indices (converts from 1-based to 0-based)
+        assertEquals(0, Parser.parseTaskIndex("mark 1", "mark "));
+        assertEquals(4, Parser.parseTaskIndex("delete 5", "delete "));
+        
+        // Test invalid inputs
+        assertEquals(-1, Parser.parseTaskIndex("mark abc", "mark "));
+        assertEquals(-1, Parser.parseTaskIndex("mark 0", "mark "));
+        assertEquals(-1, Parser.parseTaskIndex("mark ", "mark "));
+    }
+}
---
+++ src/test/java/zen/task/TaskListTest.java
@@ -0,0 +1,73 @@
+package zen.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import zen.exception.ZenException;
+
+/**
+ * JUnit tests for the TaskList class
+ */
+public class TaskListTest {
+    private TaskList taskList;
+    private Task task1;
+    private Task task2;
+
+    @BeforeEach
+    public void setUp() throws ZenException {
+        taskList = new TaskList();
+        task1 = new Todo("Read book");
+        task2 = new Todo("Write report");
+    }
+
+    @Test
+    public void addTask_validTask_increasesSize() throws ZenException {
+        assertEquals(0, taskList.size());
+        taskList.addTask(task1);
+        assertEquals(1, taskList.size());
+        assertEquals("Read book", taskList.getTask(0).getDescription());
+    }
+
+    @Test
+    public void deleteTask_validIndex_removesTask() throws ZenException {
+        taskList.addTask(task1);
+        taskList.addTask(task2);
+        
+        Task deletedTask = taskList.deleteTask(0);
+        assertEquals(task1, deletedTask);
+        assertEquals(1, taskList.size());
+        assertEquals("Write report", taskList.getTask(0).getDescription());
+    }
+
+    @Test
+    public void deleteTask_invalidIndex_throwsException() throws ZenException {
+        taskList.addTask(task1);
+        
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            taskList.deleteTask(-1);
+        });
+        
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            taskList.deleteTask(1);
+        });
+    }
+
+    @Test
+    public void markTask_validIndex_marksTaskAsDone() throws ZenException {
+        taskList.addTask(task1);
+        assertFalse(task1.isDone());
+        
+        taskList.markTask(0);
+        assertTrue(task1.isDone());
+    }
+
+    @Test
+    public void markTask_invalidIndex_throwsException() throws ZenException {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            taskList.markTask(0);
+        });
+    }
+}
